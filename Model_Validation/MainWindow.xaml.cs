2ea7750c2a569d76b3c6d05d7c48af69a90b0451
j a <james.wanders@gmail.com>
2018-06-07 08:54:43 -0700

    Up to analysis

--- Model_Validation/MainWindow.xaml.cs
+++ Model_Validation/MainWindow.xaml.cs
@@ -14,6 +14,9 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using VMS.TPS.Common.Model.API;
 using VMS.TPS.Common.Model.Types;
+using System.IO;
+using Microsoft.Win32;
+using System.Text.RegularExpressions;
 
 namespace Model_Validation
 {
@@ -26,6 +29,7 @@ namespace Model_Validation
 
         Patient pat = null;
         Course c = null;
+        List<DataScan> ds_list = new List<DataScan>();
         public MainWindow()
         {
             InitializeComponent();
@@ -81,8 +85,104 @@ namespace Model_Validation
 
         private void getScan_btn_Click(object sender, RoutedEventArgs e)
         {
-
+            ds_list.Clear();
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.DefaultExt = ".asc";
+            ofd.Filter = "Ascii Files (*.asc)|*.asc|Text Files (*.txt)|*.txt|W2CAD Files (*.cdp)|*.cdp";
+            if (ofd.ShowDialog() == true)
+            {
+                // read all scans
+                Regex newscan_re = new Regex("^\\$STOM");
+                Regex fieldsize_re = new Regex("^%FLSZ");
+                Regex scantype_re = new Regex("^%TYPE");
+                Regex pnts_re = new Regex("^%PNTS");
+                Regex step_re = new Regex("^%STEP");
+                Regex depth_re = new Regex("^%DPTH");
+                Regex data_re = new Regex("^<");
+                
+                foreach (string line in File.ReadAllLines(ofd.FileName))
+                {
+                    if (newscan_re.IsMatch(line))
+                    {
+                        //if (cur_scan != null) { ds_list.Add(cur_scan); }
+                        ds_list.Add(new DataScan());
+                    }
+                    else if (fieldsize_re.IsMatch(line))
+                    {
+                        //char[] delimiters = { ' ', '*' };
+                        string[] tokens = line.Split(' ', '*');
+                        ds_list.Last().FieldX = Convert.ToDouble(tokens[1]);
+                        ds_list.Last().FieldY = Convert.ToDouble(tokens[2]);
+                    }
+                    else if (scantype_re.IsMatch(line))
+                    {
+                        switch(line.Split(' ').Last())
+                        {
+                            case "OPP":
+                                ds_list.Last().axisDir = "X";
+                                break;
+                            case "OPD":
+                                ds_list.Last().axisDir = "Z";
+                                break;
+                            case "DPR":
+                                ds_list.Last().axisDir = "X";
+                                break;
+                        }
+                    }
+                    else if (pnts_re.IsMatch(line))
+                    {
+                        ds_list.Last().scanLength = Convert.ToInt32(line.Split(' ').Last());
+                    }
+                    else if (step_re.IsMatch(line))
+                    {
+                        ds_list.Last().stepSize = Convert.ToInt32(line.Split(' ').Last());
+                    }
+                    else if (depth_re.IsMatch(line))
+                    {
+                        ds_list.Last().depth = Convert.ToInt32(line.Split(' ').Last());
+                    }
+                    else if (data_re.IsMatch(line))
+                    {
+                        double pos = ds_list.Last().axisDir == "X" ?
+                            Convert.ToDouble(line.Split(' ').First().Trim('<')) :
+                            Convert.ToDouble(line.Split(' ')[2]);
+                        double val = Convert.ToDouble(line.Split(' ').Last().Trim('>'));
+                        ds_list.Last().scan_data.Add(new Tuple<double, double>(pos, val));
+                    }
+                }
+                prevScans_sp.Children.Clear();
+                foreach(DataScan ds in ds_list)
+                {
+                    Label lbl = new Label();
+                    string scan_type = ds.axisDir == "X" ? "Profile" : "PDD";
+                    string depth_type = ds.axisDir == "X" ? ds.depth.ToString() : "NA";
+                    lbl.Content = String.Format("{0} -FLSZ ({1} x {2}) - Depth ({3})",
+                        scan_type, ds.FieldX, ds.FieldY, depth_type);
+                    prevScans_sp.Children.Add(lbl);
+                }
+            }
         }
 
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            app.ClosePatient();
+            app.Dispose();
+        }
+
+        public class DataScan
+        {
+            public double FieldX { get; set; }
+            public double FieldY { get; set; }
+            public double StartPos { get; set; }
+            public double stepSize { get; set; }
+            public int scanLength { get; set; }
+            public string axisDir { get; set; }
+            public double depth { get; set; }
+            public List<Tuple<double, double>> scan_data { get; set; }
+            public DataScan()
+            {
+                scan_data = new List<Tuple<double, double>>();
+            }
+        }
     }
 }
